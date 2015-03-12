5cc4de6b6b2e8c2fc75ea703db14a74e34cedfeb
Scully87 <scully87.ts@googlemail.com>
2015-03-12 11:15:10 +0000

    compiled tsc type.ts changed source in html

--- type.js
+++ type.js
@@ -1,6 +1,5 @@
-function area(shape: string, width: number, height: number) {
+function area(shape, width, height) {
     var area = width * height;
     return "I'm a " + shape + " with an area of " + area + " cm squared.";
 }
-
 document.body.innerHTML = area("rectangle", 30, 15);
