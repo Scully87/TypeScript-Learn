b4dc35a9acebe42d21a1a4b7958055b8c0129909
Scully87 <scully87.ts@googlemail.com>
2015-03-12 15:38:31 +0000

    playing with 'this' added timeout

--- interface.ts
+++ interface.ts
@@ -13,3 +13,20 @@ function area(shape : Shape) {
 console.log( area( {name: "rectangle", width: 30, height: 15} ) );
 console.log( area( {name: "square", width: 30, height: 30, color: "blue"} ) );
 console.log( area( {name: "twat", width: 30, height: 15} ) );
+
+
+var shape = {
+    name: "rectangle",
+    popup: function() {
+
+        console.log('This inside popup(): ' + this.name);
+
+        setTimeout( () => {
+            console.log('This inside setTimeout(): ' + this.name);
+            console.log("I'm a " + this.name + "!");
+        }, 3000);
+
+    }
+};
+
+shape.popup();
